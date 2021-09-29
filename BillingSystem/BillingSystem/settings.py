7915c50f789e479425d2fcf72966edb9c8ba83e5
pramit2000 <pramitdutta27@gmail.com>
2021-09-30 00:28:16 +0530

    Update settings.py

--- BillingSystem/BillingSystem/settings.py
+++ BillingSystem/BillingSystem/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'BillingSystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['Templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
