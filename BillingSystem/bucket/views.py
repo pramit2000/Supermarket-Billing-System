2812a8d88a88f445ee522ed9d0410a9a251a7e28
sayantan sadhukhan <38125603+sayantan2000@users.noreply.github.com>
2021-09-30 22:04:01 +0530

    index,css,models
    logic to fetch products & show it on templates

--- BillingSystem/bucket/views.py
+++ BillingSystem/bucket/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import products
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    items=products.objects.all()
+    return render(request,'index.html',{"items":items})
