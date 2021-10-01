d3a5b3b873ba4cd2298e64b322df738b382f8b34
pramit2000 <pramitdutta27@gmail.com>
2021-10-01 23:58:36 +0530

    user signup

--- BillingSystem/user/urls.py
+++ BillingSystem/user/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from .import views
 urlpatterns = [
- path('',views.login,name="login"),
+ path('',views.index,name="index"),
  path('signup/',views.signup,name="signup"),
 ]
--- BillingSystem/user/views.py
+++ BillingSystem/user/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 # Create your views here.
-def login(request):
+def index(request):
     return render(request,'index.html')
 
 def signup(request):
-    return render(request,'signup.html')
+    if request.method == 'GET':
+        return render(request,'signup.html')
+    elif request.method == 'POST':
+        return redirect('/')
+
