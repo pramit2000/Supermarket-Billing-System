1b58068b18e2bcaee2aa5e9cf1b1d6b23d0cc9c3
pramit2000 <pramitdutta27@gmail.com>
2021-10-05 15:28:23 +0530

    test1

--- BillingSystem/user/urls.py
+++ BillingSystem/user/urls.py
@@ -4,6 +4,6 @@ from .import views
 urlpatterns = [
  path('',views.home,name="home"),
  path('signup/',views.signup,name="signup"),
- #path('signup/',views.signup,name="signup"),
+ path('checkUser/',views.checkUser,name="checkUser"),
 ]
 
