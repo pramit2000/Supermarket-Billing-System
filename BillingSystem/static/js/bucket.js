bd2234a4bccbd6cf36fce7d226eab3ed966c785f
sayantan sadhukhan <38125603+sayantan2000@users.noreply.github.com>
2021-10-08 11:06:02 +0530

    table changes

--- BillingSystem/static/js/bucket.js
+++ BillingSystem/static/js/bucket.js
@@ -1,11 +1,18 @@
 function test() {
    let table=document.getElementById("table");
-   let row = table.insertRow(1);
+   let tbody=document.getElementById("tbody")
+   let row = tbody.insertRow(1);
    let cell1=row.insertCell(0);
    let cell2=row.insertCell(1);
+   let cell3=document.getElementById("quantity").innerHTML;
+   let cell4=cell3.insertCell(2);
    let p_name=document.getElementById("p-name").innerHTML;
    let p_price=document.getElementById("p-price").innerHTML;
+   let quant=
     cell1.innerHTML=p_name;
     cell2.innerHTML=p_price;
+    cell4.innerHTML=1;
+
+
     alert("added");
 }
