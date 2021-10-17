d0ff13e49374556c1d7f6b287d8e70b20edb5080
pramit2000 <pramitdutta27@gmail.com>
2021-10-17 20:49:19 +0530

    test

--- BillingSystem/static/js/signup.js
+++ BillingSystem/static/js/signup.js
@@ -1,18 +1,25 @@
 var lengthOfPasswordError = document.getElementById("invalidPassword");
+var matchPassword = document.getElementById("matchPassword");
 
 function lengthOfPassword(){
-
 		var length = document.getElementById("inputPassword4").value.length;
 
 		if(length < 8)
-            lengthOfPasswordError.innerHTML = "     password should be minimum of 8 char";
+            lengthOfPasswordError.innerHTML = "password should be minimum of 8 char";
 
         else if (length > 12)
-            lengthOfPasswordError.innerHTML = "     password should not be more than of 12 char";
+            lengthOfPasswordError.innerHTML = "password should not be more than of 12 char";
         else
             lengthOfPasswordError.innerHTML = "";
 }
 
 function matchPassword(){
-
+    var confirmPassword = document.getElementById("inputConfirmPassword4");
+    var password = document.getElementById("inputPassword4");
+    alert("okk");
+    if(password.value != confirmPassword.value){
+        matchPassword.innerHTML = "Password is not matching"
+    }
+    else
+    matchPassword.innerHTML = ""
 }
