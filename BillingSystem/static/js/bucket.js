cdd282676ca8eaba6e40113975e54941022ba76d
pramit2000 <pramitdutta27@gmail.com>
2021-10-17 23:27:21 +0530

    bucket

--- BillingSystem/static/js/bucket.js
+++ BillingSystem/static/js/bucket.js
@@ -1,4 +1,4 @@
-function add_item() {
+function add_item(event) {
    let table=document.getElementById("table");
    let tbody=document.getElementById("tbody");
    let row = tbody.insertRow(0);
@@ -6,13 +6,27 @@ function add_item() {
    let cell2=row.insertCell(1);
    let cell4=row.insertCell(2);
 
-   let p_name=document.getElementById("p-name").innerHTML;
+   //let p_name=document.getElementById("p-name").innerHTML;
+   const p_name=document.querySelectorAll("#p-name");
+   const buttons = event.currentTarget;
+   console.log(JSON.stringify(p_name));
+   if(p_name){
+   Object.keys(p_name).forEach(element => {
+    //console.log("Inside if", typeof p_name);
+    console.log("Data: ", p_name[element].dataset.product);
+        if(buttons.dataset.product == p_name[element].dataset.product){
+        console.log("buttons.dataset.product"+ buttons.dataset.product, "p_name[element].dataset.product"+p_name[element].dataset.product);
+        console.log(p_name[element]);
+        cell1.innerHTML = p_name[element].dataset.product;
+        }
+   });
+   }
    let p_price=document.getElementById("p-price").innerHTML;
    let quant=1;
-    cell1.innerHTML=p_name;
+    //cell1.innerHTML=p_name;
     cell2.innerHTML=p_price;
     cell4.innerHTML=quant;
    
 
-    alert("added");
+   // alert("added");
 }
