65fb8405c314bb173a58b922aaf03d79a0780186
sayantan sadhukhan <sayantansk13@gmail.com>
2022-01-26 14:25:14 +0530

    linking payment page f

--- BillingSystem/bucket/views.py
+++ BillingSystem/bucket/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from .models import products
-
+total=0
 # Create your views here.
 def bucket(request):
     items=products.objects.all()
+    total= request.POST['total']
     return render(request,'bucket.html',{"items":items})
+    
+    
 def payment(request):
-    return render(request,'payment.html')
+   # total=request.GET.get('total')
+        return render(request,'payment.html',{"total":total})
     
     
