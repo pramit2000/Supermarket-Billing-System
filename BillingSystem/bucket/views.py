0daba6716a6d4bfbc4f3495ef21e34fda6d44ea6
sayantan sadhukhan <sayantansk13@gmail.com>
2022-01-28 10:26:38 +0530

    puu

--- BillingSystem/bucket/views.py
+++ BillingSystem/bucket/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 from .models import products
-total=0
+
 # Create your views here.
 def bucket(request):
     items=products.objects.all()
-    total= request.POST['total']
     return render(request,'bucket.html',{"items":items})
     
     
 def payment(request):
    # total=request.GET.get('total')
-        return render(request,'payment.html',{"total":total})
+        return render(request,'payment.html')
     
     
